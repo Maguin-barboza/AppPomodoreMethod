458b00555acee6c7b77d8b338e762a5b8f6fe731
Maguin-barboza <magno_barboza@hotmail.com>
2020-01-14 17:40:08 -0300

    Adicionar arquivos de projeto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppPomodoreMethod
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            BtnPlay.IsEnabled = false;
+            BtnPause.IsEnabled = true;
+            BtnStop.IsEnabled = true;
+
+        }
+
+        private void BtnPause_Click(object sender, RoutedEventArgs e)
+        {
+            BtnPause.IsEnabled = false;
+            BtnPlay.IsEnabled = true;
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            BtnStop.IsEnabled = false;
+            BtnPause.IsEnabled = false;
+            BtnPlay.IsEnabled = true;
+        }
+    }
+}
