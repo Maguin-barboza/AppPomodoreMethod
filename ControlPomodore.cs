14c5d2150b143e72211801cd25683211f80cf669
Maguin-barboza <magno_barboza@hotmail.com>
2020-02-19 11:58:39 -0300

    .

--- ControlPomodore.cs
+++ ControlPomodore.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AppPomodoreMethod
+﻿namespace AppPomodoreMethod
 {
     public class ControlPomodore
     {
@@ -12,5 +8,7 @@ namespace AppPomodoreMethod
         {
 
         }
+
+
     }
 }
