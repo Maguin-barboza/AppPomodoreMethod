ea3e7a3e64e398e06b49bc540bd5e6293ed84139
Maguin-barboza <magno_barboza@hotmail.com>
2020-02-24 14:42:33 -0300

    Contagem funcionando.

--- ControlPomodore.cs
+++ ControlPomodore.cs
@@ -6,28 +6,43 @@ namespace AppPomodoreMethod
     public class ControlPomodore
     {
         public Pomodore Cronometro { get; set; } = new Pomodore();
+        
         private Thread Contagem;
         private TimeSpan Segundos = new TimeSpan();
+        private bool IsPaused = false;
 
         public ControlPomodore()
         {
             Contagem = new Thread(new ThreadStart(IniciarContagem));
+            Contagem.IsBackground = true;
             Cronometro.Timer = TimeSpan.FromMinutes(25);
             Segundos = TimeSpan.FromSeconds(1);
         }
 
         public void Play()
         {
-            Contagem.Start();
+            if (!IsPaused)
+            {
+                Contagem.Start();
+            }
+            else
+            {
+                //Retornar contagem
+                //pesquisar sobre semaforos Threads
+                IsPaused = false;
+            }
+
         }
 
         public void Pause()
         {
-
+            IsPaused = true;
         }
 
         public void Stop()
         {
+            //pesquisar meios de parar a execução de uma thread.
+            //Contagem é reiniciada (retornar para o tempo definido.
             //Contagem.Abort();
         }
 
